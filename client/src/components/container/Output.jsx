dd22a27d60ff3e2d2ef9d6b9a47083f2caf443f5
RyoKojima <ryokojima@kojima.com>
2019-02-15 20:49:15 +0900

    create transaction interface

--- client/src/components/container/Output.jsx
+++ client/src/components/container/Output.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ConsoleCard from '../presentational/ConsoleCard';
 import ContractCard from '../presentational/ContractCard';
-
-const styles = theme => ({
-})
+import { ethers } from 'ethers';
 
 class Output extends Component {
     state = {
-        rpcUrl: 'http://127.0.0.1:7545'
+        rpcUrl: 'http://127.0.0.1:7545',
+        methodIdentifiers: {},
+        txInterface: []
     }
 
     handleChange = event => {
@@ -17,12 +16,96 @@ class Output extends Component {
         this.setState({rpcUrl: event.target.value})
     }
 
-    handleSubmit = () => {
-        this.props.deploy(this.state.rpcUrl)
+    handleSubmit = event => {
+        this.deploy()
+    }
+
+    deploy = async() => {
+        if(this.props.isCorrect !== true) return;
+
+        const { contracts } = this.props.compiledCode;
+
+        try {
+            let provider = new ethers.providers.JsonRpcProvider(this.state.rpcUrl)
+            let accounts = await provider.listAccounts()
+            let signer = provider.getSigner(accounts[0])
+            let factory =  ethers.ContractFactory.fromSolidity(
+                contracts['test.sol'].SimpleStorage,
+                signer
+            );
+
+            const deployer = await factory.deploy()
+            await deployer.deployed()
+
+            await this.generateTxInterface(contracts['test.sol'].SimpleStorage)
+        } catch (error) {
+            alert(`Failed.`);
+        }
+    }
+
+    generateTxInterface = async(contract) => {
+        let txInterface = []
+        await contract.abi.map((method, index) => {
+            var methodName;
+            let args = []
+
+            if (method.inputs.length > 0) {
+                methodName = method.name + '('
+
+                for(var i=0; i < method.inputs.length; i++) {
+                    let type = method.inputs[i].type
+                    methodName = methodName + type + ','
+
+                    args.push({
+                        name:method.inputs[i].name, 
+                        type:method.inputs[i].type, 
+                        value:''
+                    })
+                }
+
+                methodName = methodName.replace(/,$/, ')')
+            } else {
+                methodName = method.name + '()'
+            }
+
+            txInterface.push({
+                name:methodName, 
+                inputs:args,
+                signature: contract.evm.methodIdentifiers[methodName],
+                stateMutability: method.stateMutability
+            })
+            return txInterface
+        })
+
+        console.log(txInterface)
+        this.setState({
+            txInterface: this.state.methods.concat(txInterface)
+        })
+    }
+
+    changeArgument = event => {
+        if(event.target.value.match(/[\s\t]/)) return;
+
+        let methodId = Number(event.target.parentElement.parentElement.dataset.id)
+        let inputId = Number(event.target.id)
+
+        let methods = this.state.methods.concat()
+        let args = [...methods[methodId].inputs]
+        args[inputId].value = event.target.value
+
+        methods[methodId].args = args
+        this.setState({methods}, console.log(this.state.methods))
+    }
+
+    getMethod = event => {
+        let methodId = event.target.parentElement.dataset.id
+    }
+
+    runTransaction = async(method, inputs) => {
     }
 
     render(){
-        const { classes, isCorrect } = this.props;
+        const { isCorrect } = this.props
 
         return(
             <Grid container>
@@ -34,11 +117,14 @@ class Output extends Component {
                         rpcUrl={this.state.rpcUrl}/>
                 </Grid>
                 <Grid item xs={12}>
-                    <ContractCard />
+                    <ContractCard 
+                        handleChange={this.changeArgument}
+                        handleSubmit={this.getMethod}
+                        txInterface={this.state.txInterface}/>
                 </Grid>
             </Grid>
         );
     };
 }
 
-export default withStyles(styles)(Output);
+export default Output;
